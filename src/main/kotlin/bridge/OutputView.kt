a063cb441d1fb59fa84041cf44126567cd1f43b8
gaeun <geun5744@gmail.com>
2023-12-13 18:44:17 +0900

    [FEAT(OutputView)] 게임 결과 출력 로직 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -42,5 +42,8 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printResult() {}
+    fun printResult(isSuccess:Boolean) {
+        print("게임 성공 여부: ")
+        if (isSuccess) println("성공") else println("실패")
+    }
 }
