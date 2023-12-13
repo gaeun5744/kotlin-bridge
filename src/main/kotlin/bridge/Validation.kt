ff846b3e1f4c00a4907c29d5d35d6459d4dd8e36
gaeun <geun5744@gmail.com>
2023-12-13 18:38:41 +0900

    [FEAT(Validation)] 다리 길이 입력 유효성 검증 함수 구현

---
+++ src/main/kotlin/bridge/Validation.kt
@@ -0,0 +1,32 @@
+package bridge
+
+object Validation {
+
+    private const val NUM_ERROR_MESSAGE = "[ERROR] 숫자만 입력 가능합니다."
+    private const val NUM_RANGE_ERROR_MESSAGE = "[ERROR] 3에서 20 숫자만 입력 가능합니다."
+    private const val INVALID_MOVE_ERROR_MESSAGE = "[ERROR] U 혹은 D만 입력 가능합니다."
+    private const val INVALID_ENDING_ERROR_MESSAGE = "[ERROR] Q 혹은 R만 입력 가능합니다."
+    private const val MINIMUM_SIZE_NUM = 3
+    private const val MAX_SIZE_NUM = 20
+
+    fun checkBridgeSize(input: String) {
+        checkIsDigit(input)
+        checkNumRange(input)
+    }
+
+
+    private fun checkIsDigit(input: String) {
+        try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(NUM_ERROR_MESSAGE)
+        }
+    }
+
+    private fun checkNumRange(input: String) {
+        require(input.toInt() in MINIMUM_SIZE_NUM..MAX_SIZE_NUM) {
+            NUM_RANGE_ERROR_MESSAGE
+        }
+    }
+}
+
