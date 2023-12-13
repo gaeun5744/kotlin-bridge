d10cf38df5e9cc7548352695212c43b0224452c5
gaeun <geun5744@gmail.com>
2023-12-13 19:20:48 +0900

    [REFACTOR(BridgeGame)] 필드에서 class 파라미터로 변경

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -3,17 +3,11 @@ package bridge
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-class BridgeGame {
+class BridgeGame(private val bridge: List<String>) {
 
-    private lateinit var bridge: List<String>
     private val results = mutableListOf<String>()
     var tryNum = 1
         private set
-
-
-    fun setBridge(bridge: List<String>) {
-        this.bridge = bridge
-    }
 
     fun getResults() = results.toList()
 
@@ -39,7 +33,7 @@ class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun retry() {
-        tryNum +=1
+        tryNum += 1
         results.clear()
     }
 }
