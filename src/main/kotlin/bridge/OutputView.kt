b05d4a07f536153bc2802902822555b6bb85badb
gaeun <geun5744@gmail.com>
2023-12-13 18:40:10 +0900

    [FEAT(OutputView)] 현재까지 이동한 다리의 상태 출력 구현

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -9,7 +9,33 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(bridge: List<String>, result: List<String>) {
+
+        var upResult = "["
+        repeat(result.size) { idx ->
+            upResult += if (bridge[idx] == "U" && result[idx] == "O") {
+                " O |"
+            } else if (bridge[idx] == "D" && result[idx] == "X") { // U X
+                " X |"
+            } else {
+                "   |"
+            }
+        }
+
+        println("${upResult.dropLast(1)}" + "]")
+
+        var downResult = "["
+        repeat(result.size) { idx ->
+            downResult += if (bridge[idx] == "D" && result[idx] == "O") {
+                " O |"
+            } else if (bridge[idx] == "U" && result[idx] == "X") {
+                " X |"
+            } else {
+                "   |"
+            }
+        }
+        println("${downResult.dropLast(1)}" + "]")
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
